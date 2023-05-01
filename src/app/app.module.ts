ce331fb8ee8ee399fec62721fce5177d8ede25dd
Mohsen Dahesh <mdehesh@gmail.com>
2023-05-01 13:09:48 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './state/counter.reducer';
+import { MyCounterComponent } from './my-counter/my-counter/my-counter.component';
+import { booksReducer } from './state/books.reducer';
+import { collectionReducer } from './state/collection.reducer';
+import { BookListComponent } from './book-list/book-list/book-list.component';
+import { BookCollectionComponent } from './book-collection/book-collection.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyCounterComponent,
+    BookListComponent,
+    BookCollectionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({count:counterReducer, books:booksReducer, collection:collectionReducer})
   ],
   providers: [],
   bootstrap: [AppComponent]
